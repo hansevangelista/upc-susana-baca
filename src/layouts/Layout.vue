d3a68faeb4aacf5ba8ab37b05246f8a095708e30
Diter Evan <hansevangelista@gmail.com>
2024-10-28 17:22:21 -0500

    add changes

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -2,7 +2,7 @@
   <div class="auth">
     <slot></slot>
 
-    <audio autoplay loop>
+    <audio autoplay loop volume=0.2>
     <source src="../assets/intro/music.mp3" type="audio/mpeg"> <!-- Added background audio -->
       Your browser does not support the audio element.
     </audio>
